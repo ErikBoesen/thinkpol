997a1add844e0f43edfc1cabb9214f0f3dfc16ff
ErikBoesen <me@erikboesen.com>
2018-02-10 16:16:25 -0500

    Clean up output, write to stdout, format as markdown

--- pol.py
+++ pol.py
@@ -1,20 +1,25 @@
 import discord
 import datetime
 import argparse
+import sys
 
 
 class Listener(discord.Client):
     async def on_ready(self):
-        print('-- Surveillance started --')
-        for server in self.servers:
-            print('Detecting thoughtcrime on server %s' % server.name)
+        sys.stdout.write('Surveillance started!\n')
+        sys.stdout.write('=====================\n')
+        sys.stdout.write('**Logged in as %s#%s**  \n' % (self.user.name, self.user.discriminator))
+        sys.stdout.write('Monitoring servers %s\n' % ', '.join(['`%s`' % server.name for server in self.servers]))
+        sys.stdout.write('---------\n')
+        sys.stdout.flush()
 
     async def on_message(self, msg):
-        print('%s/%s#%s->%s | %s' % (msg.timestamp,
-                                     msg.server.name if msg.server else 'DM',
-                                     msg.channel.name,
-                                     msg.author.name,
-                                     msg.content))
+        sys.stdout.write('- **%s/%s#%s->%s** %s' % (msg.timestamp,
+                                                    msg.server.name if msg.server else 'DM',
+                                                    msg.channel.name,
+                                                    msg.author.name,
+                                                    msg.content))
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
